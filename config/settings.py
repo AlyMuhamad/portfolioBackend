75164e7b0713f21b5a513a2ab864add5129a31bc
Aly Mohamed <alimoh0801@gmail.com>
2025-12-09 07:59:05 +0200

    Fix bug

--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,7 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
