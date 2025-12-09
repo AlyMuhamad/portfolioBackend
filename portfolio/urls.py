06ffb0584c5cd973f88520e1c68b11bf7aa9fb2e
Aly Mohamed <alimoh0801@gmail.com>
2025-12-09 07:26:20 +0200

    Fix bug

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import getRoutes, getProjects
 
 urlpatterns = [
     path('api/', getRoutes, name='getRoutes'),
     path('api/projects/', getProjects, name='getProjects'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)
